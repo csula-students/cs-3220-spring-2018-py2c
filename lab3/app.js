eb3f288d124eb0877f81fd465195193fda5cc4da
Flap jack <stingbear@sbcglobal.net>
2018-02-24 19:18:02 -0800

    changing more files

---
+++ lab3/app.js
@@ -0,0 +1,36 @@
+
+class PubSub {
+    constructor () {
+        this.subscribers = [];
+    }
+
+    // subscribe allows a new subscriber to listen for changes by providing
+    // callback function in the parameter
+    subscribe (fn) {
+        this.subscribers.push(fn);
+    }
+
+    // one can publish any data to subscribers
+    publish (data) {
+        this.subscribers.forEach(subscriber => {
+            subscriber(data);
+        });
+    }
+}
+
+
+const pubSub = new PubSub();
+
+
+const dom = document.querySelector('#click-me');
+dom.addEventListener('click',()=> {
+
+pubSub.publish(window.incrementalGame.state.counter);
+
+});
+
+pubSub.subscribe(counter => {
+counter ++;
+window.incrementalGame.state.counter = counter;
+document.getElementById("counts").innerHTML = "Rupee's : " + counter ;
+});
