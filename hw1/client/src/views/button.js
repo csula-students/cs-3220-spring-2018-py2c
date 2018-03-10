7771048ef963f55708b25ec63971ebdb6d863d40
Flap jack <stingbear@sbcglobal.net>
2018-03-09 22:25:45 -0800

    starting it up

---
+++ hw1/client/src/views/button.js
@@ -0,0 +1,33 @@
+import constants from '../constants';
+export default function (store) {
+	return class ButtonComponent extends window.HTMLElement {
+		constructor () {
+			super();
+			this.store = store;
+			
+		}
+			
+
+			// TODO: add click event to increment counter
+			// hint: use "store.dispatch" method (see example component)
+		
+
+		
+	connectedCallback(){
+		console.log('ButtonComponentt#onConnectedCallback');
+		this.innerHTML = '<button>Click me</button>';
+		this.addEventListener('click', () => {
+			this.store.dispatch({
+				type: 'BUTTON_CLICK'
+			});
+		});
+		}
+	
+
+	disconnectedCallback () {
+		console.log('ButtonComponent#onDisconnectedCallback');
+		this.store.unsubscribe(this.onStateChange);
+}
+
+	};
+}
