c708239dd1e43dbef6029fff8797e57dd83f2bc3
Flap jack <stingbear@sbcglobal.net>
2018-03-12 00:41:50 -0700

    undid my error

---
+++ hw1/client/src/models/story.js
@@ -0,0 +1,36 @@
+export default class Story {
+	/**
+	 * create a new story based on the meta passed in argument
+	 * @constructor
+	 * @param {object} meta - the meta data for story
+	 */
+	constructor (meta) {
+		this.name = meta.name;
+		this.description = meta.description;
+		this.triggeredAt = meta.triggeredAt;
+		this.state = meta.state;
+	}
+
+	/**
+	 * isUnlockYet checks if this story is ready to be unlocked yet
+	 * @param {number} value - the resource value at the moment
+	 * @return {boolean} if this story is unlockable
+	 */
+	isUnlockYet (value) {
+		if (value >= this.triggeredAt && this.state === 'hidden'){
+			return true;
+		}else
+		{
+			return false;
+		}
+		
+	}
+
+	/**
+	 * unlock simply unlock the story to visible state
+	 */
+	unlock () {
+		this.state = 'visible';
+		// TODO: change the story state to "visible"
+	}
+}
