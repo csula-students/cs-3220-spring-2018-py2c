173bf684ada266b1f566977308a3d26c4279615f
Flap jack <stingbear@sbcglobal.net>
2018-03-24 18:50:22 -0700

    reverting stuff back

--- src/main/java/edu/csula/web/HelloServlet.java
+++ src/main/java/edu/csula/web/HelloServlet.java
@@ -14,5 +14,6 @@ public class HelloServlet extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("<h1>Hello world!</h1>");
+		
 	}
 }
