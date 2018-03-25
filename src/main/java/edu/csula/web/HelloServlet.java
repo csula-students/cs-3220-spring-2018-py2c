378c2fda2859470d42166b3763fd8d2fa00cdfb2
Flap jack <stingbear@sbcglobal.net>
2018-03-24 18:28:01 -0700

    changing it back

--- src/main/java/edu/csula/web/HelloServlet.java
+++ src/main/java/edu/csula/web/HelloServlet.java
@@ -2,39 +2,17 @@ package edu.csula.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Collection;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/guestbook")
-public class GuestbookServlet extends HttpServlet {
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
 	public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		String html = "<h1>Hello Guestbook! </h1>";
-		Collection<GuestboookEntry> entries = (Collection<GuestbookEntry>) getServletContext().getAttribute("guestbook-entries");
-			html += "<table border ='1'>";
-			 for (GuestbookEntry entry : entries)
-			 {
-				 html += "<tr>";
-				 html += "<td>" + entry.getUsername() + "says </td><td>" + entry.getComment() + "</td><td>Edit | Delete</td>";
-				 html += "</tr>";
-			
-			 }
-			 html += "</table>";
-			 html += "<form>";
-			 html += "<h2>Add Comment</h2>";
-			 html += "<label for ='name'>Name:</label>";
-			 html += "<input name = 'username   id = 'name type 'text />";
-			 html += "< textarea  name 'comment' ></textarea>";
-			 html += "<button>Submit</button>";
-			 html += "</form>";
-
-
-		out.println(html);
+		out.println("<h1>Hello world!</h1>");
 	}
 }
