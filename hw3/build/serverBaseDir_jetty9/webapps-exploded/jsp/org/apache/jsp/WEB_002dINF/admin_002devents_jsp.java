aef3ba16477ffa8af5d3329774ef633a9d684531
Flap jack <stingbear@sbcglobal.net>
2018-05-05 14:13:10 -0700

    hw3

---
+++ hw3/build/serverBaseDir_jetty9/webapps-exploded/jsp/org/apache/jsp/WEB_002dINF/admin_002devents_jsp.java
@@ -0,0 +1,240 @@
+package org.apache.jsp.WEB_002dINF;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class admin_002devents_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_forEach_var_items;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_c_forEach_var_items = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_c_forEach_var_items.release();
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\r\n");
+      out.write("<html lang=\"en\">\r\n");
+      out.write("\r\n");
+      out.write("<head>\r\n");
+      out.write("\r\n");
+      out.write("    <meta charset=\"UTF-8\">\r\n");
+      out.write("\r\n");
+      out.write("    <title>Incremental game framework - game information</title>\r\n");
+      out.write("\r\n");
+      out.write("    <link rel=\"stylesheet\" href=\"../app.css\">\r\n");
+      out.write("\r\n");
+      out.write("</head>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<body class =\"width-constraint\">\r\n");
+      out.write("\r\n");
+      out.write("    <header>\r\n");
+      out.write("\r\n");
+      out.write("        <h1>Incremental game framework</h1>\r\n");
+      out.write("\r\n");
+      out.write("        <nav class=\"main-nav\">\r\n");
+      out.write("\r\n");
+      out.write("            <a href=\"game-info.html\">Game Information</a>\r\n");
+      out.write("\r\n");
+      out.write("            | \r\n");
+      out.write("\r\n");
+      out.write("            <a href=\"generators\">Generators</a>\r\n");
+      out.write("\r\n");
+      out.write("            | \r\n");
+      out.write("\r\n");
+      out.write("            <a href=\"events\">Events</a>\r\n");
+      out.write("\r\n");
+      out.write("        </nav>\r\n");
+      out.write("\r\n");
+      out.write("    </header>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<main>\r\n");
+      out.write("\r\n");
+      out.write("    <div class=\"row-container\">\r\n");
+      out.write("\r\n");
+      out.write("        <form class=\"fill card\" method= \"POST\">\r\n");
+      out.write("\r\n");
+      out.write("            <div class=\"form-group\">\r\n");
+      out.write("\r\n");
+      out.write("                <label for=\"name\">Event Name</label>\r\n");
+      out.write("\r\n");
+      out.write("                <input type=\"text\" id=\"names\" name =\"name\">\r\n");
+      out.write("\r\n");
+      out.write("            </div>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("            <div class=\"form-group\">\r\n");
+      out.write("\r\n");
+      out.write("                <label for=\"description\">Event Description</label>\r\n");
+      out.write("\r\n");
+      out.write("                <textarea id=\"description\"name =\"descTextArea\" ></textarea>\r\n");
+      out.write("\r\n");
+      out.write("            </div>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("            <div class=\"form-group\">\r\n");
+      out.write("\r\n");
+      out.write("                <label for=\"trigger_at\">Trigger at</label>\r\n");
+      out.write("\r\n");
+      out.write("                <input type=\"number\" id=\"trigger_at\" name =\"triggerInput\">\r\n");
+      out.write("\r\n");
+      out.write("            </div>\r\n");
+      out.write("\r\n");
+      out.write("            <div class=\"actions\">\r\n");
+      out.write("\r\n");
+      out.write("                <button>Save</button>\r\n");
+      out.write("\r\n");
+      out.write("            </div>\r\n");
+      out.write("\r\n");
+      out.write("        </form>\r\n");
+      out.write("\r\n");
+      out.write("        <div>\r\n");
+      out.write("\r\n");
+      out.write("            <table class=\"info-table table-with-elipsis\">\r\n");
+      out.write("\r\n");
+      out.write("                <thead>\r\n");
+      out.write("\r\n");
+      out.write("                    <tr>\r\n");
+      out.write("\r\n");
+      out.write("                        <th>Name</th>\r\n");
+      out.write("\r\n");
+      out.write("                        <th>Description</th>\r\n");
+      out.write("\r\n");
+      out.write("                        <th>Trigger Point</th>\r\n");
+      out.write("\r\n");
+      out.write("                        <th>Action</th>\r\n");
+      out.write("\r\n");
+      out.write("                    </tr>\r\n");
+      out.write("\r\n");
+      out.write("                </thead>\r\n");
+      out.write("\r\n");
+      out.write("                <tbody>\r\n");
+      out.write("                        ");
+      if (_jspx_meth_c_forEach_0(_jspx_page_context))
+        return;
+      out.write("\r\n");
+      out.write("                </tbody>\r\n");
+      out.write("\r\n");
+      out.write("            </table>\r\n");
+      out.write("\r\n");
+      out.write("        </div>\r\n");
+      out.write("\r\n");
+      out.write("    </div>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("</main>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("</body>\r\n");
+      out.write("\r\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+
+  private boolean _jspx_meth_c_forEach_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:forEach
+    org.apache.taglibs.standard.tag.rt.core.ForEachTag _jspx_th_c_forEach_0 = (org.apache.taglibs.standard.tag.rt.core.ForEachTag) _jspx_tagPool_c_forEach_var_items.get(org.apache.taglibs.standard.tag.rt.core.ForEachTag.class);
+    _jspx_th_c_forEach_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_forEach_0.setParent(null);
+    _jspx_th_c_forEach_0.setItems((java.lang.Object) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${events}", java.lang.Object.class, (PageContext)_jspx_page_context, null));
+    _jspx_th_c_forEach_0.setVar("event");
+    int[] _jspx_push_body_count_c_forEach_0 = new int[] { 0 };
+    try {
+      int _jspx_eval_c_forEach_0 = _jspx_th_c_forEach_0.doStartTag();
+      if (_jspx_eval_c_forEach_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+        do {
+          out.write("\r\n");
+          out.write("                                <tr>\r\n");
+          out.write("                                    \r\n");
+          out.write("                                    <td>");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${event.getName()}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("</td>\r\n");
+          out.write("                                    <td>");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${event.getDescription()}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("</td>\r\n");
+          out.write("                                    <td>");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${event.getTriggerAt()}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("</td>\r\n");
+          out.write("                                    <td>\r\n");
+          out.write("                                            <a href=\"EditEventServlet?id=");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${event.getId()}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("\">Edit</a>|\r\n");
+          out.write("                                            <a href=\"DeleteEventServlet?id=");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${event.getId()}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("\">Delete</a> \r\n");
+          out.write("                                    </td>\r\n");
+          out.write("                                </tr>\t\r\n");
+          out.write("                                ");
+          int evalDoAfterBody = _jspx_th_c_forEach_0.doAfterBody();
+          if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+            break;
+        } while (true);
+      }
+      if (_jspx_th_c_forEach_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_c_forEach_0[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_c_forEach_0.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_c_forEach_0.doFinally();
+      _jspx_tagPool_c_forEach_var_items.reuse(_jspx_th_c_forEach_0);
+    }
+    return false;
+  }
+}
